f751cb7726aa0985e9196dde1bcd0df8c0210472
Mo-sk <58175449+Mo-sk@users.noreply.github.com>
2020-06-20 17:49:26 +0200

    entreprise-show

--- resources/views/Contacts/create.blade.php
+++ resources/views/Contacts/create.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/dist/create.css"/>
+    <link rel="stylesheet" href="../css/dist/create.css"/>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200&display=swap" rel="stylesheet">
     <title>Création d'un contact</title>
 </head>
@@ -25,7 +25,7 @@
 
         <label for="email">Email : </label>
         <input id="email" type="text" name="email" required>
-        <label for="email_verified_at">Email verifier : </label>
+        <label for="email_verified_at">Vérifier votre Email : </label>
         <input id="email_verified_at" type="text" name="email_verified_at" required>
 
         <label for="entreprise">Entreprise :</label>
--- resources/views/Entreprises/show.blade.php
+++ resources/views/Entreprises/show.blade.php
@@ -4,21 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/css/dist/show.css"/>
+    <link rel="stylesheet" href="../../css/dist/show.css"/>
     <title>Affichage d'une entreprise</title>
 </head>
 <body>
-    @if(!is_null($entreprise))
-        <h1>{{ $entreprise->name }}</h1>
+    <div class="container">
+        @if(!is_null($entreprise))
+            <h1>{{ $entreprise->name }}</h1>
 
-        <div>
-            <p><strong>Adresse :</strong> {{ $entreprise->adresse }}</p>
-            <p><strong>Type :</strong> {{ $entreprise->type }}</p>
-        </div>
-    @else
-        <p>l'entreprise n'existe pas</p>
-    @endif
+            <div class="logo">
+                <img src="img/{{  $entreprise->url }}" alt="logo-entreprise">
+            </div>
 
-    <a href="{{ route('entreprises.index') }}">Retour a l'accueil</a>
+            <div class="adresse">
+                <p><strong>Adresse :</strong> {{ $entreprise->adresse }}</p>
+                <p><strong>Type :</strong> {{ $entreprise->type }}</p>
+            </div>
+        @else
+            <p>l'entreprise n'existe pas</p>
+        @endif
+            <div class="retour">
+                <a href="{{ route('entreprises.index') }}">Retour a l'accueil</a>
+            </div>
+    </div>
 </body>
 </html>
